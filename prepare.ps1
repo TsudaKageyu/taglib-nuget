9c8e58c630c8053f4532216a3a551bb3c6bf33d3
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2015-06-05 16:38:21 +0900

    Build and add binaries for VS2015 RC.

--- prepare.ps1
+++ prepare.ps1
@@ -11,7 +11,7 @@ $ErrorActionPreference = "Stop"
 # MSBuild Settings
 
 Set-Variable -Name Toolsets -Option Constant -Value @(
-    "v90", "v100", "v110", "v120"
+    "v90", "v100", "v110", "v120", "v140"
 )
 
 Set-Variable -Name Platforms -Option Constant -Value @(
